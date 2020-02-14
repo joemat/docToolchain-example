c12b94e83149c148043e076db65bd82b96fcbf85
Jörg Matysiak <joemat@elinja.de>
2020-02-14 22:39:59 +0100

    * added diagrams

--- builddoc.sh
+++ builddoc.sh
@@ -15,11 +15,11 @@ docker create --entrypoint /bin/bash \
        --name "${CONTAINER_NAME}" \
        rdmueller/doctoolchain:rc-1.2.0 \
        -c "doctoolchain doc $CMD $@ -PinputPath=. \
-       -PmainConfigFile=doc/Config.groovy"
+       -PmainConfigFile=Config.groovy"
 
 docker cp src/main/doc "${CONTAINER_NAME}:/project"
 
-docker start "${CONTAINER_NAME}"
+docker start -a "${CONTAINER_NAME}"
 docker wait "${CONTAINER_NAME}"
 docker cp "${CONTAINER_NAME}:/project/doc/build" .
 docker rm "${CONTAINER_NAME}"
